13a6545f6925135c1551e498de5b1e08b5253dfa
SOFTMAXTER <softmaxter@hotmail.com>
2025-09-04 21:11:03 -0600

    Run.bat

--- Run.bat
+++ Run.bat
@@ -30,7 +30,7 @@ echo Privilegios de Administrador obtenidos. Ejecutando Aegis Phoenix Suite...
 echo.
 
 :: Establecer el titulo de la ventana
-title Aegis Phoenix Suite v4.6.1 by SOFTMAXTER
+title Aegis Phoenix Suite v4.6.2 by SOFTMAXTER
 
 :: Ruta al script de PowerShell (asume que esta en una carpeta llamada SCRIPT)
 set "scriptPath=%~dp0Script\AegisPhoenixSuite.ps1"
